937daeda3266eec1ee83313fe4ed18cbebb943af
Daniel Rivas <ers.daniel+dev@gmail.com>
2016-05-31 10:36:49 -0400

    Updated config for digital ocean server

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :portfolio, Portfolio.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "blooming-headland-10991.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "danielrs.me"],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
