d3b9fb991305ab66336c01ad83e88f8594c8b807
Daniel Rivas <ers.daniel+dev@gmail.com>
2017-02-08 17:49:29 -0600

    Added info logs to check at server

--- lib/portfolio/mailer.ex
+++ lib/portfolio/mailer.ex
@@ -3,7 +3,12 @@ defmodule Portfolio.Mailer do
     domain: Application.get_env(:portfolio, :mailgun_domain),
     key: Application.get_env(:portfolio, :mailgun_key)
 
+    require Logger
+
   def send_contact_email(name, email, subject, text) do
+    Logger.info Application.get_env(:portfolio, :mailgun_domain)
+    Logger.info Application.get_env(:portfolio, :mailgun_key)
+
     send_email to: "info@danielrs.me",
                from: email,
                subject: name <> " - " <> subject,
