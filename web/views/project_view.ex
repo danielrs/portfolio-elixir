7193521eb1e4f1c9e8bfa3aaf09b804357d4f612
Daniel Rivas <ers.daniel+dev@gmail.com>
2017-02-22 00:15:35 -0600

    Better projects cards

--- web/views/project_view.ex
+++ web/views/project_view.ex
@@ -1,3 +1,9 @@
 defmodule Portfolio.ProjectView do
   use Portfolio.Web, :view
+  alias Portfolio.Utils
+
+  def band_class(string) do
+    "project-card__band--color-"
+    <> (Utils.codify(string) |> rem(10) |> Integer.to_string)
+  end
 end
